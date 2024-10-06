72503f87b9a3761a3d3979622e143ee45d5c733e
Paul Barthmaier <pbrocks@users.noreply.github.com>
2024-10-06 18:19:44 -0400

    updates

--- includes/local-image-filters.php
+++ includes/local-image-filters.php
@@ -3,21 +3,23 @@
 defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
 
 $options = get_option( 'tag_options' );
+if ( is_array( $options ) && isset( $options['production_domain'] ) ) {
+	// Access the array value
+	$production_domain = esc_attr( $options['production_domain'] );
 
-$production_domain = esc_attr( $options['production_domain'] );
+	if ( isset( $production_domain ) && $production_domain !== $_SERVER['HTTP_HOST'] ) {
+		add_filter(
+			'pre_option_upload_path',
+			function ( $upload_path ) {
+				return '/wp-content/uploads/';
+			}
+		);
 
-if ( isset( $production_domain ) && $production_domain !== $_SERVER['HTTP_HOST'] ) {
-    add_filter(
-        'pre_option_upload_path',
-        function ( $upload_path ) {
-            return '/wp-content/uploads/';
-        }
-    );
-
-    add_filter(
-        'pre_option_upload_url_path',
-        function ( $upload_url_path ) {
-            return '//' . $production_domain . '/wp-content/uploads/';
-        }
-    );
+		add_filter(
+			'pre_option_upload_url_path',
+			function ( $upload_url_path ) {
+				return '//' . $production_domain . '/wp-content/uploads/';
+			}
+		);
+	}
 }
--- save-wp-env-path.sh
+++ save-wp-env-path.sh
@@ -1,8 +1,3 @@
-# wpdev-local-plugin
-
-## Bash file to create docker path
-
-```sh
 # ./save-wp-env-path.sh
 #!/bin/bash
 # Ensure the directory exists
@@ -11,9 +6,3 @@ mkdir -p ./local/uploads/info-to-pass
 # Save the WP_ENV path to the file
 echo "Saving wp-env path..."
 echo $WP_ENV_PATH > ./local/uploads/info-to-pass/wp-env-path.txt
-
-```
-
-## Make sure it's executable
-
-    chmod +x save-wp-env-path.sh
