81518ee34e8211372401cd24e3240259228b2316
Paul Barthmaier <pbrocks@users.noreply.github.com>
2024-10-07 07:52:09 -0400

    Update local-image-filters.php

--- includes/local-image-filters.php
+++ includes/local-image-filters.php
@@ -7,17 +7,17 @@ if ( is_array( $options ) && isset( $options['production_domain'] ) ) {
 	// Access the array value
 	$production_domain = esc_attr( $options['production_domain'] );
 
-	if ( isset( $production_domain ) && $production_domain !== $_SERVER['HTTP_HOST'] ) {
+	if ( isset( $production_domain ) ) {
 		add_filter(
 			'pre_option_upload_path',
-			function ( $upload_path ) {
+			function () {
 				return '/wp-content/uploads/';
 			}
 		);
 
 		add_filter(
 			'pre_option_upload_url_path',
-			function ( $upload_url_path ) {
+			function ( $production_domain ) {
 				return '//' . $production_domain . '/wp-content/uploads/';
 			}
 		);
