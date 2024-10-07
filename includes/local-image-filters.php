8eada3015429baccbf9b7bd2cd96217eae01f0d7
Paul Barthmaier <pbrocks@users.noreply.github.com>
2024-10-07 13:52:37 -0400

    Update local-image-filters.php

--- includes/local-image-filters.php
+++ includes/local-image-filters.php
@@ -4,20 +4,22 @@ defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
 
 $options = get_option( 'tag_options' );
 
-$production_domain = esc_attr( $options['production_domain'] );
+if ( is_array( $options ) && isset( $options['production_domain'] ) ) {
+	$production_domain = esc_attr( $options['production_domain'] );
 
-if ( isset( $production_domain ) && $production_domain !== $_SERVER['HTTP_HOST'] ) {
-    add_filter(
-        'pre_option_upload_path',
-        function ( $upload_path ) {
-            return '/wp-content/uploads/';
-        }
-    );
+	if ( $production_domain !== $_SERVER['HTTP_HOST'] ) {
+	    add_filter(
+	        'pre_option_upload_path',
+	        function ( $upload_path ) {
+	            return '/wp-content/uploads/';
+	        }
+	    );
 
-    add_filter(
-        'pre_option_upload_url_path',
-        function ( $upload_url_path ) {
-            return '//' . $production_domain . '/wp-content/uploads/';
-        }
-    );
+	    add_filter(
+	        'pre_option_upload_url_path',
+	        function ( $upload_url_path ) use ( $production_domain ) {
+	            return "//$production_domain/wp-content/uploads/";
+	        }
+	    );
+	}
 }
